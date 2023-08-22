fd1aa9534177b646fddb89ef71425f00e3bfd566
scastd <scastd00@gmail.com>
2023-08-22 19:11:09 +0200

    Update raygui-cpp namespace for ease of use
    Changed the defined namespace of the raygui-cpp library from raylib::gui to rgc.

--- include/raygui-cpp/Directives.h
+++ include/raygui-cpp/Directives.h
@@ -1,10 +1,12 @@
 #ifndef RAYGUI_CPP_DIRECTIVES_H
 #define RAYGUI_CPP_DIRECTIVES_H
 
-#define RAYGUI_CPP_NAMESPACE raylib::gui
+// Defines the namespace of the library as rgc (raylib gui cpp)
+// Less typing for the user
+#define RAYGUI_CPP_NAMESPACE rgc
 
 #define RAYGUI_CPP_BEGIN_NAMESPACE namespace RAYGUI_CPP_NAMESPACE {
-#define RAYGUI_CPP_END_NAMESPACE } // namespace raylib::gui
+#define RAYGUI_CPP_END_NAMESPACE }
 
 #define RAYGUI_NODISCARD [[nodiscard]]
 
