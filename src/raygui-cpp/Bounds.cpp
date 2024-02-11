b6766bce609bcfd03dc2013bad256b4dae0af41c
scastd <scastd00@gmail.com>
2024-02-11 15:20:09 +0100

    Optimized WindowBounds function in Bounds class
    The WindowBounds function in the Bounds class has been optimized to improve code efficiency. Introducing a static variable inside the function reduces the number of object constructs and deliveries, effectively enhancing the overall performance.

--- src/raygui-cpp/Bounds.cpp
+++ src/raygui-cpp/Bounds.cpp
@@ -51,7 +51,8 @@ void Bounds::SetHeight(float newHeight) {
 }
 
 Bounds Bounds::WindowBounds() {
-    return { 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) };
+    static Bounds windowBounds = { 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) };
+    return windowBounds;
 }
 
 Bounds Bounds::OfSize(float width, float height) {
